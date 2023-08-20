f1281cbb0167d17428f8e84f767e1cc8a78cde65
AnviAggarwal13 <anviaggarwal13@gmail.com>
2023-08-20 23:14:39 +0530

    updates

--- scripts/postslist.js
+++ scripts/postslist.js
@@ -3,21 +3,21 @@ let targetElement;
 
 function openDeletePostModal(e){
     targetElement = e.parentElement.parentElement.parentElement.parentElement;
-    const modal= document.getElementById("post-modal");
+    const modal= document.getElementById("delete-post-modal");
     modal.style.display="flex";
 }
 
 function hideDeletePostModal(){
     targetElement=null;
-    const modal=document.getElementById("post-modal");
+    const modal=document.getElementById("delete-post-modal");
     modal.style.display="none";
 }
 
 function showFullPost(e) {
-    let post = e.parentElement.parentElement.parentElement;
-    let userName = post.getElementsByTagName("p")[0].innerHTML;
-    let title = post.getElementsByTagName("p")[1].innerHTML;
-    let postContent = post.getElementsByTagName("p")[2].innerHTML;
+    let posts = e.parentElement.parentElement.parentElement;
+    let userName = posts.getElementsByTagName("p")[0].innerHTML;
+    let title = posts.getElementsByTagName("p")[1].innerHTML;
+    let postContent = posts.getElementsByTagName("p")[2].innerHTML;
     sessionStorage.setItem("title", title);
     sessionStorage.setItem("userName", userName);
     sessionStorage.setItem("postContent", postContent);
@@ -26,6 +26,6 @@ function showFullPost(e) {
 }
 
 function deletePost() {
-    document.getElementById("post-modal").style.display = "none";
+    document.getElementById("delete-post-modal").style.display = "none";
     targetElement.remove();
   }
